4c4521043e4ac8be59fe1bbdcebd3cd482367448
RyanStone2640 <ryan880822@gmail.com>
2022-05-11 11:27:54 +0800

    1-4 使用js內建模組，nodejs內建模組

---
+++ 03builtInModule.js
@@ -0,0 +1,9 @@
+// nodejs內建module
+const path = require("path");
+
+// js內建模組
+console.log('dirname', __dirname);
+console.log('filename', __filename);
+
+// 使用nodejs內建module path，顯示檔案路徑
+console.log(path.join(__dirname, "03builtInModule.js"));
